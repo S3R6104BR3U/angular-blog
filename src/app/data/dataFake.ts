cbc00847f14271925cbe9de2ce097fcacb3be269
S3R6104BR3U <sergioabreusousa@gmail.com>
2023-05-18 13:09:28 -0300

    content create

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,31 @@
+export const dataFake = [
+{
+	"id":"1",
+	"title": "Novo Game de Megaman X",
+	"description": "Capcom anuncia novo jogo de Megaman X.",
+	"photoCover": "https://www.finalfaqs.com.br/wp-content/uploads/2019/11/images.jpg"
+},
+
+{
+	"id":"2",
+	"title": "Resident Evil 9",
+	"description": "Resident Evil 9 Já está em desenvolvimento",
+	"photoCover": "https://i.ytimg.com/vi/CINjzUcqk2Y/maxresdefault.jpg"
+},
+{
+	"id":"3",
+	"title": "Devil My Cry 5",
+	"description": "Dante está de volta!",
+	"photoCover": "https://i.ytimg.com/vi/4uAGiXow1Vw/maxresdefault.jpg"
+},
+
+{
+	"id":"4",
+	"title": "Exoprimal",
+	"description": "Capcom 'aposta' em aventura Jurassica",
+	"photoCover": "https://cdn.promo.capcomusa.com/screenshots/117152-3.jpg"
+},
+
+
+
+	]
